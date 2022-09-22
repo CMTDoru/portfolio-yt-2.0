ebacde66d778a1cfe46166b38b0cdbbeee602c61
CMTDoru <doru0186@yahoo.com>
2022-09-22 17:24:55 +0300

    Added experience section.

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,7 @@
 import type { NextPage } from "next";
 import Head from "next/head";
 import About from "../components/About";
+import WorkExperience from "../components/WorkExperience";
 import Header from "../components/Header";
 import Hero from "../components/Hero";
 
@@ -22,9 +23,10 @@ const Home: NextPage = () => {
       <section id="about" className="snap-center">
         <About />
       </section>
-
       {/* Experience */}
-
+      <section id="experience" className="snap-center">
+        <WorkExperience />
+      </section>
       {/* Skills */}
 
       {/* Projects */}
